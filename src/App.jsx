e843b2ac9fbaf9e4f58dd3a228f0e0cb99b67da3
Anoruo Benjamin <82318783+Benjamin4041@users.noreply.github.com>
2024-11-15 18:06:20 +0000

    update3

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Canvas  camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}>
+      <Canvas  camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }} shadows>
         <Experience />
       </Canvas>
     </>
--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,26 +1,29 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useControls } from 'leva'
 import React from 'react'
 
 export default function Experience() {
 
 
+    const { positions } = useControls({ positions: { value: { x: 2, y: 0,  } } })
+    
     return (<>
         <OrbitControls />
-        <Perf />
+        <Perf position='top-left' />
         <ambientLight />
-        <directionalLight position={[0, 0, 0]} />
-        <mesh position={[-2,0,0]}>
-            <sphereGeometry  />
+        <directionalLight position={[1, 2, 3]} castShadow intensity={1.5} />
+        <mesh position={[-2,0,0]} castShadow>
+            <sphereGeometry />
             <meshStandardMaterial color={'orange'} />
         </mesh>
-        <mesh position={[2, 0, 0,]}>
+        <mesh position={[2,0,0]} castShadow >
             <boxGeometry />
             <meshBasicMaterial color={'purple'} />
         </mesh>
-        <mesh position={[0, -1, 0]} rotation-x={- Math.PI * 0.5} scale={10}>
+        <mesh position={[0, -1, 0]} rotation-x={- Math.PI * 0.5} scale={10} receiveShadow>
             <planeGeometry />
-            <meshBasicMaterial color={"lightgreen"} />
+            <meshStandardMaterial color={"lightgreen"} />
         </mesh>
     </>)
 }
