e460083e878f0713c626fc40d0524a575f07078f
Anoruo Benjamin <82318783+Benjamin4041@users.noreply.github.com>
2024-11-15 15:25:37 +0000

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
-import { Perf } from 'r3f-perf'
+
 import Experience from './components/experience'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <Canvas className='can' camera={[0,0,0]}>
+      <Canvas  camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}>
         <Experience />
       </Canvas>
     </>
--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,18 +6,21 @@ export default function Experience() {
 
 
     return (<>
-    <OrbitControls/>
+        <OrbitControls />
         <Perf />
         <ambientLight />
         <directionalLight position={[0, 0, 0]} />
-        <mesh>
-            <sphereGeometry />
+        <mesh position={[-2,0,0]}>
+            <sphereGeometry  />
             <meshStandardMaterial color={'orange'} />
         </mesh>
-        <mesh position={[2,0,0,]}>
-            <boxGeometry/>
-            <meshBasicMaterial color={'purple'}/>
+        <mesh position={[2, 0, 0,]}>
+            <boxGeometry />
+            <meshBasicMaterial color={'purple'} />
         </mesh>
-
+        <mesh position={[0, -1, 0]} rotation-x={- Math.PI * 0.5} scale={10}>
+            <planeGeometry />
+            <meshBasicMaterial color={"lightgreen"} />
+        </mesh>
     </>)
 }
