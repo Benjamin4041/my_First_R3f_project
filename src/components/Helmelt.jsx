a4e66438c1e3c0292927ace628ddcb3815afe707
Anoruo Benjamin <82318783+Benjamin4041@users.noreply.github.com>
2024-11-16 17:08:13 +0000

    update10

---
+++ src/components/Helmelt.jsx
@@ -0,0 +1,16 @@
+import React, { Suspense } from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { useLoader } from '@react-three/fiber'
+export default function Helmelt() {
+    const model = useLoader(GLTFLoader, './assets/3dmodels/FlightHelmet/glTF/FlightHelmet.gltf', (loader) => {
+        const dracoLoader = new DRACOLoader()
+        dracoLoader.setDecoderPath('./assets/3dmodels/draco/')
+        loader.setDRACOLoader(dracoLoader)
+    })
+    return (<>
+        <Suspense>
+            <primitive object={model.scene} scale={5} position-y={-1} />
+        </Suspense>
+    </>)
+}
--- src/components/experience2.jsx
+++ src/components/experience2.jsx
@@ -1,26 +1,20 @@
 // loading 3D Objects
 
 import { OrbitControls } from '@react-three/drei'
-import { useFrame, useLoader } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
 import React, { useRef } from 'react'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-
-
+import Helmelt from './Helmelt'
 
 
 export default function Experience2() {
     const burger = useRef()
-    const model = useLoader(GLTFLoader,'./assets/3dmodels/hamburger.glb')
-    useFrame((state,delta)=>{
-        burger.current.rotation.y +=delta
-    })
+
     return (<>
         <OrbitControls />
         <Perf position='top-left' />
         <directionalLight position={[1, 2, 3]} intensity={1.5} castShadow />
         <ambientLight intensity={0.5} />
-            <primitive object={model.scene} scale={0.35} ref={burger} />
+        <Helmelt />
         <mesh scale={10} position-y={-1} rotation-x={- Math.PI * 0.5}>
             <planeGeometry />
             <meshStandardMaterial color={'greenyellow'} />
